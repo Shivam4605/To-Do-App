9f17d6741110c1f44a7e6554ec4398656c5c8e36
Shivam Kolekar <shivamkolekar8@gmail.com>
2025-09-27 23:27:10 +0530

    fifth commit

--- lib/Database/database.dart
+++ lib/Database/database.dart
@@ -32,7 +32,7 @@ class HelperDatabase {
   }
 
   // this method is used to insert the data into the Database
-  ///[insertData]
+  ///[insertData] for
   void insertData(Map<String, dynamic> obj) async {
     Database localDB = await createDB();
     await localDB.insert(
